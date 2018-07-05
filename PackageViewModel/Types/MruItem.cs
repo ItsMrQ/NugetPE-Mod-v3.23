5591fcce484656ce72440088d571d6df050679b0
ItsMrQ <qgjevukaj@gmail.com>
2018-07-05 10:56:53 -0400

    Improved/Revised functions, fixed issue involving version changes with buttons (+/-) being saved during a Cancel Edit of Metadata.

--- PackageViewModel/Types/MruItem.cs
+++ PackageViewModel/Types/MruItem.cs
@@ -13,6 +13,7 @@ namespace NuGetPackageExplorer.Types
     {
         public string Id { get; set; }
         public TemplatebleSemanticVersion Version { get; set; }
+        public TemplatebleSemanticVersion EditableVersion { get; set; }
         public string Path { get; set; }
         public PackageType PackageType { get; set; }
 
